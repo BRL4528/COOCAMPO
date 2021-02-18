ca76a342693aa20adeb4a673c7a0b6f29eebad1e
brl4528 <blgc.sgo@hotmail.com>
2021-02-18 12:55:00 -0400

    update baseURL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: process.env.REACT_APP_API_URL,
-  baseURL: 'https://samasc.site',
+  baseURL: process.env.REACT_APP_API_URL,
+  // baseURL: 'https://samasc.site',
 });
 
 export default api;
