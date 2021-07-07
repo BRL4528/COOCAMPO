75a108a7a8e5719fcc5631b60c149f9fd1935fb2
administrator <alessandro.rds03@gmail.com>
2021-07-07 18:39:10 +0000

    Revert "update access in powerbi"
    This reverts commit c52fc6ca1e3c68ea3c678696e50dcfbd25a8e327.

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,11 +6,6 @@ export const api = axios.create({
   baseURL: 'https://digitalocean.samasc.site',
 });
 
-export const apiPowerBI = axios.create({
-  // baseURL: process.env.REACT_APP_API_URL,
-  baseURL: 'http://192.168.1.48:3333/',
-});
-
 export const apiGeninfo = axios.create({
   baseURL: 'http://geninfo.cooasgo.com.br:8081/GeninfoPPR/api/v1/',
   auth: {
