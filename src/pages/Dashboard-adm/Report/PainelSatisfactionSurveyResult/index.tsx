c99889828fa5399b2a77d67284d1cab6e6e8df40
brl4528 <blgc.sgo@hotmail.com>
2021-07-07 18:58:09 +0000

    Revert "Revert "update access in powerbi""
    This reverts commit 75a108a7a8e5719fcc5631b60c149f9fd1935fb2.

--- src/pages/Dashboard-adm/Report/PainelSatisfactionSurveyResult/index.tsx
+++ src/pages/Dashboard-adm/Report/PainelSatisfactionSurveyResult/index.tsx
@@ -1,15 +1,30 @@
-import React, { useCallback, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { Report } from 'powerbi-report-component';
 import { useLoading, Oval } from '@agney/react-loading';
 import Button from '../../../../components/Global/Button';
 
 import { CardButton, CardeHeader, Container } from './styles';
+import { apiPowerBI } from '../../../../services/api';
+import { useToast } from '../../../../hooks/toast';
+
+interface PropsPowerBI {
+  accessToken: string;
+  embedUrl: [
+    {
+      reportId: string;
+      reportName: string;
+      embedUrl: string;
+    },
+  ];
+}
 
 const SelectorFolders: React.FC = () => {
   const [loadSignInUser, setloadSignInUser] = useState(true);
-
   const componentRef = useRef<HTMLDivElement>(null);
   // const [report, embed] = useReport();
+
+  const { addToast } = useToast();
+  const [dataBI, setDataBI] = useState<PropsPowerBI>();
 
   const handleLoaded = useCallback(() => {
     setloadSignInUser(false);
@@ -19,6 +34,27 @@ const SelectorFolders: React.FC = () => {
     loading: loadSignInUser,
     indicator: <Oval />,
   });
+
+  useEffect(() => {
+    async function loadTokenBI(): Promise<void> {
+      try {
+        const response = await apiPowerBI.get(
+          '/get-embed-token?reportId=49601ede-66ec-4fe2-88c0-299c21f1b1af',
+        );
+        setDataBI(response.data);
+        // console.log(response.data.embedUrl[0].reportId);
+      } catch (err) {
+        console.log(err);
+
+        addToast({
+          type: 'error',
+          title: 'Gráficos de analíse',
+          description: 'Não foi posivel carregar os gráficos de analíse',
+        });
+      }
+    }
+    loadTokenBI();
+  }, [addToast]);
 
   const layoutSettings = {
     width: '100%',
@@ -96,9 +132,9 @@ const SelectorFolders: React.FC = () => {
         <div id="print">
           <Report
             tokenType="Aad" // "Aad"
-            accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Im5PbzNaRHJPRFhFSzFqS1doWHNsSFJfS1hFZyIsImtpZCI6Im5PbzNaRHJPRFhFSzFqS1doWHNsSFJfS1hFZyJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvY2Y1ZDI0YTMtNGU3ZS00ZjgzLWIzY2QtYTk2MzkwYjE2MGFmLyIsImlhdCI6MTYyNTUxMjExMiwibmJmIjoxNjI1NTEyMTEyLCJleHAiOjE2MjU1MTYwMTIsImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBVlFBcS84VEFBQUFiam1KRk9KZ0VpOGJhSlpRT3NDNk1FMUtlRFNPUWRySDBrTlgxTXRYaGgwT1VlWGFhQVBOOTdRRkZMTW1YcXUrcXhoNm5tVitwQ1pjekg2WGVXUUtTRlBWQlNxeG9IQnhwYzBkaWZWSzNJOD0iLCJhbXIiOlsicHdkIiwibWZhIl0sImFwcGlkIjoiODcxYzAxMGYtNWU2MS00ZmIxLTgzYWMtOTg2MTBhN2U5MTEwIiwiYXBwaWRhY3IiOiIyIiwiaXBhZGRyIjoiMTc3LjIwMS42Ny4xNzgiLCJuYW1lIjoiUEJJIEVtYmVkIiwib2lkIjoiYTAyZDQ0MWUtYjAzNy00OGM0LTk3ZDctNTQyZWZhNWFhZTRiIiwicHVpZCI6IjEwMDMyMDAxMTA4NjYyREUiLCJyaCI6IjAuQVZvQW95UmR6MzVPZzAtenphbGprTEZncnc4QkhJZGhYckZQZzZ5WVlRcC1rUkJhQVBZLiIsInNjcCI6InVzZXJfaW1wZXJzb25hdGlvbiIsInN1YiI6IjUwSDR0TlQ5ZU1qSDhRMGw1aEQ0SVVEdi1McERHYTI0ZnBjTndqcnpWakEiLCJ0aWQiOiJjZjVkMjRhMy00ZTdlLTRmODMtYjNjZC1hOTYzOTBiMTYwYWYiLCJ1bmlxdWVfbmFtZSI6InBiaWVtYmVkQGJydW5vZ3VpbWFyYWVzY2FydmFsaG9zZ29oby5vbm1pY3Jvc29mdC5jb20iLCJ1cG4iOiJwYmllbWJlZEBicnVub2d1aW1hcmFlc2NhcnZhbGhvc2dvaG8ub25taWNyb3NvZnQuY29tIiwidXRpIjoiazNGZmZudXYyMFd2TFNLb2Z5LUNBZyIsInZlciI6IjEuMCIsIndpZHMiOlsiYjc5ZmJmNGQtM2VmOS00Njg5LTgxNDMtNzZiMTk0ZTg1NTA5Il19.EpeINaJ89xhjqjiNTEeJ7HUN7umF85TInINX1TIMK8kIcTq_ceJ0dTnK5q2J96JaRiObmGgfFJ_ua_k7lt-n2bAXJbiztyUzu6QW9AuNQdPItzSVUiTp4qieQcFccG973uRHTCaoyUOw90_BAX5s75EqoyrTfTOIwL32InJxtsYyg5zfB0R7yw8kr8bmyfCxMsf27s90gzKcuJ6ZL634gENlqPX7FFS7GZ6XOE41oTyB3sOarwUIasJNfe5NIoEXQaLJl6n8XeoS2eN3kSA_SfrogRSWV_Htxt_XHRREMLC3UZtdfoYWU0z8MLzfRyH6X2fXdm3pe79Ci0Xr_RD3MA" // accessToken goes here
-            embedUrl="https://app.powerbi.com/reportEmbed?reportId=49601ede-66ec-4fe2-88c0-299c21f1b1af&groupId=9ab7f913-45e8-4e73-abf4-33ff01c21f9f&w=2&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLUJSQVpJTC1TT1VUSC1CLVBSSU1BUlktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQiLCJlbWJlZEZlYXR1cmVzIjp7Im1vZGVybkVtYmVkIjp0cnVlLCJjZXJ0aWZpZWRUZWxlbWV0cnlFbWJlZCI6dHJ1ZSwidXNhZ2VNZXRyaWNzVk5leHQiOnRydWV9fQ%3d%3d" // embedUrl goes here
-            embedId="49601ede-66ec-4fe2-88c0-299c21f1b1af" // report or dashboard Id goes here
+            accessToken={dataBI ? dataBI.accessToken : 'sem token'}
+            embedUrl={dataBI ? dataBI.embedUrl[0].embedUrl : 'sem token'}
+            embedId={dataBI ? dataBI.embedUrl[0].reportId : 'sem token'}
             // pageName="Sentiment" // set as current page of the report
             reportMode="View" // open report in a particular mode View/Edit/Create
             // datasetId={datasetId} // required for reportMode = "Create" and optional for dynamic databinding in `report` on `View` mode
