7516aa77c489807243983b863c92a7b0fb1c7f09
brl4528 <blgc.sgo@hotmail.com>
2021-07-26 15:17:30 +0000

    update style button config in sidebar

--- src/components/Admin/Modal/styles.ts
+++ src/components/Admin/Modal/styles.ts
@@ -1,3 +1,8 @@
 import styled from 'styled-components';
 
-export const Conatiner = styled.div``;
+export const Conatiner = styled.div`
+  font-size: rgba(0, 0, 255);
+  h1 .all {
+    background: green;
+  }
+`;
