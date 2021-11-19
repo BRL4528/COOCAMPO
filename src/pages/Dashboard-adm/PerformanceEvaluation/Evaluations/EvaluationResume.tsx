485a08284cb2abe0bdec6e35bed7a7c3e0f712e8
brl4528 <blgc.sgo@hotmail.com>
2021-11-18 20:15:03 -0400

    update performance evalution

--- src/pages/Dashboard-adm/PerformanceEvaluation/Evaluations/EvaluationResume.tsx
+++ src/pages/Dashboard-adm/PerformanceEvaluation/Evaluations/EvaluationResume.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Box,
   Text,
@@ -8,8 +8,44 @@ import {
   Button,
   ScaleFade,
 } from '@chakra-ui/react';
+import { api } from '../../../../services/api';
+
+interface Evaluations {
+  id: string;
+  name: string;
+  description: string;
+  status: Boolean;
+  performances: [
+    {
+      id: string;
+      name: string;
+      weight: string;
+    },
+  ];
+}
 
 export default function EvaluationResume() {
+  const [dataEvaluation, setDataEvaluation] = useState<Evaluations[]>();
+  useEffect(() => {
+    api.get('/factors').then(response => {
+      const array: Evaluations[] = [];
+      response.data.filter(async (dataEva: Evaluations) => {
+        await api.get(`performances/show?factor_id=${dataEva.id}`).then(res => {
+          const formated = {
+            id: dataEva.id,
+            name: dataEva.name,
+            description: dataEva.description,
+            status: dataEva.status,
+            performances: res.data,
+          };
+          array.push(formated);
+        });
+      });
+      console.log('aaaa', array);
+      setDataEvaluation(response.data);
+    });
+  }, []);
+
   return (
     <ScaleFade initialScale={0.9} in>
       <Flex
@@ -35,73 +71,43 @@ export default function EvaluationResume() {
           </Text>
         </Box>
 
-        <Box p={['4', '8']} bg="gray.800" borderRadius={8} w="100%" mt="8">
-          <Text fontWeight="medium" fontSize="2xl">
-            Pontualidade/Assiduidade
-          </Text>
-          <Text
-            color="gray.300"
-            borderBottom="1px"
-            borderColor="gray.700"
-            pb="2"
+        {dataEvaluation?.map(data => (
+          <Box
+            key={data.id}
+            p={['4', '8']}
+            bg="gray.800"
+            borderRadius={8}
+            w="100%"
+            mt="8"
           >
-            Presença do colaborador no local de trabalho dentro do horário
-            estabelecido para o expediente da empresa.
-          </Text>
-          <RadioGroup mt="8">
-            <Radio value="10" m="5" cursor="pointer">
-              Cumpre o horário e está sempre presente, permanece depois do
-              horário de expediente quando necessário, mostrando-se disposto a
-              atender às necessidades de trabalho.
-            </Radio>
-            <Radio value="7.5" m="5" cursor="pointer">
-              Cumpre o horário estabelecido e é pontual nos seus compromissos de
-              trabalho, não há o hábito de trabalhar fora do horário de
-              expediente.
-            </Radio>
+            <Text fontWeight="medium" fontSize="2xl">
+              {data.name}
+            </Text>
+            <Text
+              color="gray.300"
+              borderBottom="1px"
+              borderColor="gray.700"
+              pb="2"
+            >
+              {data.description}
+            </Text>
+            <RadioGroup mt="8">
+              {data.performances?.map(dataPerformance => (
+                <Box>
+                  <Radio
+                    value={`${dataPerformance.weight}`}
+                    m="5"
+                    cursor="pointer"
+                    key={dataPerformance.id}
+                  >
+                    {dataPerformance.name}
+                  </Radio>
+                </Box>
+              ))}
+            </RadioGroup>
+          </Box>
+        ))}
 
-            <Radio value="5" m="5" cursor="pointer">
-              Normalmente não cumpre o horário estabelecido, mas, quando
-              presente, atende às necessidades de trabalho.
-            </Radio>
-            <Radio value="2.5" m="5" cursor="pointer">
-              Nunca cumpre horário e está sempre ausente.
-            </Radio>
-          </RadioGroup>
-        </Box>
-        <Box p={['4', '8']} bg="gray.800" borderRadius={8} w="100%" mt="8">
-          <Text fontWeight="medium" fontSize="2xl">
-            Diciplina
-          </Text>
-          <Text
-            color="gray.300"
-            borderBottom="1px"
-            borderColor="gray.700"
-            pb="2"
-          >
-            Observa sistematicamente aos regulamentos e às normas emanadas do
-            superior imediato.
-          </Text>
-          <RadioGroup mt="8">
-            <Radio value="10" m="5" cursor="pointer">
-              Sempre cumpre as normas e deveres, além de contribuir para a
-              manutenção da ordem no ambiente de trabalho.
-            </Radio>
-            <Radio value="7.5" m="5" cursor="pointer">
-              CMantém um comportamento satisfatório atendendo às normas e
-              deveres da empresa.
-            </Radio>
-
-            <Radio value="5" m="5" cursor="pointer">
-              Eventualmente descumpre as determinações que lhes são atribuídas e
-              tem um comportamento instável no grupo.
-            </Radio>
-            <Radio value="2.5" m="5" cursor="pointer">
-              Mostra-se resistente a cumprir normas e deveres e sempre
-              influencia negativamente no comportamento do grupo.
-            </Radio>
-          </RadioGroup>
-        </Box>
         <Button mt="8" size="md" colorScheme="yellow">
           Finalizar avaliação
         </Button>
