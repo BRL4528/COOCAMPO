766c03de0059e29dc0256e2c99a91ed9849df9c3
brl4528 <blgc.sgo@hotmail.com>
2022-01-27 13:42:56 -0400

    update bugd in resposivit from menu

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -3,7 +3,7 @@
 import React, { useContext } from 'react';
 
 import { Link } from 'react-router-dom';
-import { Box, Center } from '@chakra-ui/react';
+import { Box, Center, SimpleGrid, useBreakpointValue } from '@chakra-ui/react';
 import { useAuth } from '../../hooks/auth';
 
 import { ContainerCard, Content } from './styles';
@@ -16,63 +16,72 @@ import adm from '../../assets/adm.svg';
 import { SetToggleThemeContext } from '../../contexts/SetToggleThemeContext';
 
 const Menu: React.FC = () => {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   const { user } = useAuth();
   const { toggleTheme } = useContext(SetToggleThemeContext);
 
   return (
     <ContainerCard theme={toggleTheme}>
       <Center>
-        {/* <SimpleGrid
-          minChildWidth="80px"
-          // flex="1"
-          spacing={3}
-        > */}
-        <Box className={user.tag === 'admin' ? '' : 'disabled'} m="5px">
-          <Link to="/miles/dashboard">
-            <Content>
-              <img src={destin} alt="imagem de destino" />
-              <strong>Controle de frota</strong>
-            </Content>
-          </Link>
-        </Box>
+        <SimpleGrid
+          flex="1"
+          columns={[2, null, 4]}
+          // columns={{ sm: 2, md: 4 }}
+          spacing={5}
+          // minChildWidth={['370px', '400', '420px']}
+          align="flex-start"
+          mt={isWideVersion ? '0' : '10rem'}
+        >
+          <Box className={user.tag === 'admin' ? '' : 'disabled'} m="5px">
+            <Link to="/miles/dashboard">
+              <Content>
+                <img src={destin} alt="imagem de destino" />
+                <strong>Controle de frota</strong>
+              </Content>
+            </Link>
+          </Box>
 
-        <Box m="5px">
-          <Link to="/management-ppr/dashboard">
-            <Content>
-              <img src={pie} alt="imagem de grafico pizza" />
-              <strong>Gerenciamento PPR</strong>
-            </Content>
-          </Link>
-        </Box>
+          <Box m="5px">
+            <Link to="/management-ppr/dashboard">
+              <Content>
+                <img src={pie} alt="imagem de grafico pizza" />
+                <strong>Gerenciamento PPR</strong>
+              </Content>
+            </Link>
+          </Box>
 
-        {/* <div className={user.tag === 'admin' ? '' : 'disabled'}> */}
-        <Box m="5px">
-          <Link to="/service-orders/service-integrity">
-            <Content>
-              <img src={os} alt="imagem de ordem de serviço" />
-              <strong>Ordens de serviço</strong>
-            </Content>
-          </Link>
-        </Box>
-        {/* </div> */}
+          {/* <div className={user.tag === 'admin' ? '' : 'disabled'}> */}
+          <Box m="5px">
+            <Link to="/service-orders/service-integrity">
+              <Content>
+                <img src={os} alt="imagem de ordem de serviço" />
+                <strong>Ordens de serviço</strong>
+              </Content>
+            </Link>
+          </Box>
+          {/* </div> */}
 
-        <Box className={user.tag === 'admin' ? '' : 'disabled'} m="5px">
-          <Link to="/rules">
-            <Content>
-              <img src={fluxo} alt="imagem de fluxo grama" />
-              <strong>Normas interna</strong>
-            </Content>
-          </Link>
-        </Box>
-        <Box className={user.tag === 'admin' ? '' : 'disabled'} m="5px">
-          <Link to="/administrator/employers">
-            <Content>
-              <img src={adm} alt="imagem de administrador" />
-              <strong>Administrador</strong>
-            </Content>
-          </Link>
-        </Box>
-        {/* </SimpleGrid> */}
+          <Box className={user.tag === 'admin' ? '' : 'disabled'} m="5px">
+            <Link to="/rules">
+              <Content>
+                <img src={fluxo} alt="imagem de fluxo grama" />
+                <strong>Normas interna</strong>
+              </Content>
+            </Link>
+          </Box>
+          <Box className={user.tag === 'admin' ? '' : 'disabled'} m="5px">
+            <Link to="/administrator/employers">
+              <Content>
+                <img src={adm} alt="imagem de administrador" />
+                <strong>Administrador</strong>
+              </Content>
+            </Link>
+          </Box>
+        </SimpleGrid>
       </Center>
     </ContainerCard>
   );
